0bb8e78cad331ed0bc5d372692f59bde165abf3f
MaksymPlichuk <pilipm449@gmail.com>
2026-06-17 17:49:07 +0300

    Create category

--- my_site/silpo/categories/views.py
+++ my_site/silpo/categories/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.utils.text import slugify
 
-# Create your views here.
+from categories.models import Category
+from categories.forms import CategoryForm
+from django.contrib import messages
+from users.utils import save_custom_image
+
+def category_list(request):
+    categories = Category.objects.all().order_by('name')
+    return render(request, 'categories/category_list.html', {'categories': categories} )
+
+def category_create(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            try:
+                category = form.save(commit=False)
+                if not category.slug:
+                    category.slug = slugify(category.name, allow_unicode=False)
+                if 'image' in request.FILES:
+                    image = request.FILES['image']
+                    image_name = save_custom_image(image, size=(600,600), folder='categories')
+                    category.image=image_name
+                category.save()
+                messages.success(request, "Category successfully created")
+                return redirect('categories:list')
+            except Exception as e:
+                messages.error(request, 'Category error', {str(e)})
+        else:
+            messages.error(request, "Corret errors in Form")
+    else:
+        form = CategoryForm()
+    return render(request, 'categories/category_create.html', {'form':form} )    
+
+
+
+
--- my_site/silpo/mysite/urls.py
+++ my_site/silpo/mysite/urls.py
@@ -23,6 +23,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include("users.urls")),
+    path('categories/', include("categories.urls")),
     path('', views.homepage, name="homepage")
 ]
 urlpatterns += static(settings.IMAGES_URL, document_root=settings.IMAGES_ROOT)
