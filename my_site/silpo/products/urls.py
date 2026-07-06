6e48740ea1038359730bbbc8f1da8231384253c6
Maksym <pilipm449@gmail.com>
2026-07-06 03:45:59 +0300

    added Product creation product page, image highlight

--- my_site/silpo/products/urls.py
+++ my_site/silpo/products/urls.py
@@ -5,4 +5,6 @@ app_name='products'
 
 urlpatterns=[
     path('', views.show_products, name="show_products"),
+    path('create/', views.create_product, name="create"),
+    path('<int:prod_id>/', views.show_one_product, name="one_product")
 ]
--- my_site/silpo/products/views.py
+++ my_site/silpo/products/views.py
@@ -1,8 +1,42 @@
-from django.shortcuts import render
-from products.models import Product
-
+from django.shortcuts import render, redirect, get_object_or_404
+from products.models import Product, ProductImage
+from .forms import ProductForm
+from django.contrib import messages
+from django.utils.text import slugify
 
 # Create your views here.
 def show_products(request):
     products = Product.objects.prefetch_related("images").all()
     return render(request, "products.html", {"products": products})
+
+def create_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            try:
+                product = form.save(commit=False)
+                if not product.slug:
+                    product.slug = slugify(product.name, allow_unicode=False)
+                product.save()
+                images = request.FILES.getlist('images')
+                for index, image in enumerate(images):
+                    ProductImage.objects.create(
+                        product=product,
+                        image=image,
+                        priority=index
+                    )
+                messages.success(request, 'Product created successfully')
+                return redirect('products:show_products')
+
+            except Exception as x:
+                messages.error(request, f'Помилка створення продукту {str(x)}')
+        else:
+            messages.error(request, 'Виправте помилки у формі')
+    else:
+        form = ProductForm()
+    return render(request, 'create_product.html', {'form': form})
+
+def show_one_product(request, prod_id):
+    product = get_object_or_404(Product, id=prod_id)
+    return render(request,"productPage.html", {"product":product})
+
